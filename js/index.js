9700d0ccfe58cfad99a48bd3fce2fad2f1ebf277
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-06-09 19:02:29 -0400

    added mouseover/mouseout event listener to logo h1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+let logoHead = document.querySelector('.nav-container h1');
+logoHead.addEventListener('mouseover', (e)=>{
+    e.target.style.fontSize = '4.5rem';
+})
+logoHead.addEventListener('mouseout', (e)=>{
+    e.target.style.fontSize = '4rem';
+})
