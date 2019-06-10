af1eed707bbc6aea8d811e9d755cc4180460b673
Ray Guillotte <rayguillotte@Rays-Air.fios-router.home>
2019-06-09 20:22:37 -0400

    added dblclick event to header to change background color. added click event to all img's to toggle 'large' class

--- js/index.js
+++ js/index.js
@@ -7,3 +7,44 @@ logoHead.addEventListener('mouseover', (e)=>{
 logoHead.addEventListener('mouseout', (e)=>{
     e.target.style.fontSize = '4rem';
 })
+
+let navBar = document.querySelector('header');
+
+navBar.addEventListener('dblclick', (e)=>{
+    e.target.style.backgroundColor = 'lightgrey';
+});
+
+let firstImg = document.querySelector('.content-section .img-content img');
+
+firstImg.addEventListener('click', (e)=>{
+    firstImg.classList.toggle('large');
+})
+
+let secondImg = document.querySelector('.inverse-content img');
+
+secondImg.addEventListener('click', (e)=>{
+    secondImg.classList.toggle('large');
+})
+
+let headerImg = document.querySelector('.intro img');
+
+headerImg.addEventListener('click', (e)=>{
+    headerImg.classList.toggle('large');
+})
+
+let bottomImg = document.querySelector('.content-destination img');
+
+bottomImg.addEventListener('click', (e)=>{
+    bottomImg.classList.toggle('large');
+})
+
+
+
+
+
+
+
+
+
+
+
